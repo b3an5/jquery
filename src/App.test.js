d57d375cf49105009ecfa8e2d997beeac9d4fbf5
B3an5 <beantheo@gmail.com>
2019-01-08 09:34:10 -0700

    more app tests

--- src/App.test.js
+++ src/App.test.js
@@ -10,25 +10,62 @@ it('renders without crashing', () => {
 });
 
 
+
+
 describe('App', () => {
-
+  
   let wrapper
-
+  const setDifficulty = jest.fn()
+  
   beforeEach(() => {
     wrapper = shallow(
       <App />
-    )
-  })
-
-  it('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
-  })
-
-  it('should have the correct default state', () => {
-    expect(wrapper.state()).toEqual({
-      jQueryData: null,
-      splash: true,
-      difficulty: 10
+      )
+    })
+    
+    it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    })
+    
+    it('should have the correct default state', () => {
+      expect(wrapper.state()).toEqual({
+        jQueryData: null,
+        splash: true,
+        difficulty: 10
+      })
+    })
+    
+    it('should change difficulty in state when set difficulty is called', () => {
+      expect(wrapper.state()).toEqual({
+        jQueryData: null,
+        splash: true,
+        difficulty: 10
+      })
+      wrapper.instance().setDifficulty(20);
+      expect(wrapper.state()).toEqual({
+        jQueryData: null,
+        splash: true,
+        difficulty: 20
+      })
+    })
+    
+    it('should change splash in state when set toggleMainPage is called', () => {
+      expect(wrapper.state()).toEqual({
+        jQueryData: null,
+        splash: true,
+        difficulty: 10
+      })
+      wrapper.instance().toggleMainPage();
+      expect(wrapper.state()).toEqual({
+        jQueryData: null,
+        splash: false,
+        difficulty: 10
+      })
+    })
+    
+    it.skip('shouldrun set difficulty when passDifficulty is run', () => {
+      let difficultyButtons = [{checked: true}];
+      wrapper.instance().passDifficulty();
+      expect(setDifficulty).toBeCalled()
     })
   })
-})
